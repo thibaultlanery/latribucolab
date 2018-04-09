093d12ce02a10978e3c599d434dae18942066d46
Thibault Lanery <thibaultlanery@gmail.com>
2018-04-09 15:43:39 -0500

    mailchimp automatic subscribe ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+    after_create :subscribe_to_newsletter
+
+  private
+
+  def subscribe_to_newsletter
+    SubscribeToNewsletterService.new(self).call
+  end
+
+
 end
