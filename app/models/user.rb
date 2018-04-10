193dca163f433d0dcf6fb0a0836629a54619db51
Thibault Lanery <thibaultlanery@gmail.com>
2018-04-10 10:23:13 -0500

    email check ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
 
     after_create :subscribe_to_newsletter
-
+    validates :email, presence: true, uniqueness: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
+    validates :name, presence: true
   private
 
   def subscribe_to_newsletter
