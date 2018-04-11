95e034c51469603bbe3ae25e4b6d355288267d45
Thibault Lanery <thibaultlanery@gmail.com>
2018-04-11 17:02:00 -0500

    amelioration home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :show, :create]
+  resources :users, only: [:show, :create]
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
