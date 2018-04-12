ccb34c28dd2e7f6c90fcc4f0137280ba11d2e0cf
Thibault Lanery <thibaultlanery@gmail.com>
2018-04-12 11:53:09 -0500

    modification image comment ca marche couleur orange de mm navbar et title h1

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,10 +1,25 @@
+// $( document ).ready(function {
+//   navbar.classList.remove('navbar-logo-white');
+// });
+
+// $(document).ready(function(){
+//     $(".navbar-logo-white").remove();
+// });
+
+
+
+
+ // $(document).ready(function() {
+ //            $(this).removeClass("navbar-logo-white");
+ //         });
+
 function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
   window.addEventListener('scroll', () => {
     if (window.scrollY >= window.innerHeight) {
-      navbar.classList.add('navbar-wagon-white');
+      navbar.classList.add('navbar-wagon-white', 'navbar-logo-white');
     } else {
-      navbar.classList.remove('navbar-wagon-white');
+      navbar.classList.remove('navbar-wagon-white', 'navbar-logo-white');
     }
   });
 }
