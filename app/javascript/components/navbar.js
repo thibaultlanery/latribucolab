63336cf67da1b3c67a6985ff94877884e63f7f97
Thibault Lanery <thibaultlanery@gmail.com>
2018-04-13 19:53:33 -0500

    linear gradient image cafe quinoa

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,10 +3,10 @@
 function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
   window.addEventListener('scroll', () => {
-    if (window.scrollY >= window.innerHeight) {
-      navbar.classList.add('navbar-wagon-white', 'navbar-logo-white');
+    if (window.scrollY >= 100) {
+      navbar.classList.add('navbar-wagon-white');
     } else {
-      navbar.classList.remove('navbar-wagon-white', 'navbar-logo-white');
+      navbar.classList.remove('navbar-wagon-white');
     }
   });
 }
