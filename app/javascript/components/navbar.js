b4b3c82d15a76ddb152a6ae5f50c37c37b0a1b38
Thibault Lanery <thibaultlanery@gmail.com>
2018-04-22 15:30:11 -0500

    modif couleur navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -4,9 +4,11 @@ function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
   window.addEventListener('scroll', () => {
     if (window.scrollY >= 100) {
+
       navbar.classList.add('navbar-wagon-white');
     } else {
       navbar.classList.remove('navbar-wagon-white');
+
     }
   });
 }
