be3fd50d9f0c9167bbe0e1156cbe99d64341c037
Thibault Lanery <thibaultlanery@gmail.com>
2018-04-30 15:53:32 -0500

    responsive show ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,17 @@
 Category.destroy_all
 
 category_list = [
-  ["De nouvelles variétés de café", "https://res.cloudinary.com/dcljc9muz/image/upload/c_fill,h_150,w_150/v1522885728/café2_mbn1tg.webp"],
-  ["Des variétés de thés", "http://res.cloudinary.com/dcljc9muz/image/upload/c_fill,h_150,w_150/v1523829838/DSC_0015_hifvs0.webp"],
-  ["Des variétés de poivres", "https://res.cloudinary.com/dcljc9muz/image/upload/v1523828163/DSC_0007_fcfzai.webp"],
-  ["Des variétés d'épices","https://res.cloudinary.com/dcljc9muz/image/upload/c_fill,h_150,w_150/v1523828163/DSC_0008_pthpke.webp"],
-  ["De la Vanille", "http://res.cloudinary.com/dcljc9muz/image/upload/v1523829958/van_p0rf1e.webp"],
-  ["Des variétés de lentilles","http://res.cloudinary.com/dcljc9muz/image/upload/v1523829956/lent_sb6xl7.webp"],
-  ["Des variétés de riz", "https://res.cloudinary.com/dcljc9muz/image/upload/c_fill,h_150,w_150/v1523828163/DSC_0012_hsznsq.webp" ],
-  ["Des tablettes de chocolats au vrai cacao", "http://res.cloudinary.com/dcljc9muz/image/upload/c_fill,h_150,q_auto:best,w_150/v1523828589/cacao_oprarl.webp"]
+  ["De nouvelles variétés de café", "https://res.cloudinary.com/dcljc9muz/image/upload/c_fill,h_150,w_150/v1522885728/café2_mbn1tg.png"],
+  ["Des variétés de thés", "https://res.cloudinary.com/dcljc9muz/image/upload/c_fill,h_150,w_150/v1523829838/DSC_0015_hifvs0.png"],
+  ["Des variétés de poivres", "https://res.cloudinary.com/dcljc9muz/image/upload/c_scale,h_150,q_auto:good/v1523828163/DSC_0007_fcfzai.png"],
+  ["Des variétés d'épices","https://res.cloudinary.com/dcljc9muz/image/upload/c_fill,h_150,w_150/v1523828163/DSC_0008_pthpke.png"],
+  ["De la Vanille", "https://res.cloudinary.com/dcljc9muz/image/upload/c_scale,h_150,q_auto:good/v1523829958/van_p0rf1e.png"],
+  ["Des variétés de lentilles","https://res.cloudinary.com/dcljc9muz/image/upload/c_scale,h_150,q_auto:good/v1523829956/lent_sb6xl7.png"],
+  ["Des variétés de riz", "https://res.cloudinary.com/dcljc9muz/image/upload/c_fill,h_150,w_150/v1523828163/DSC_0012_hsznsq.png" ],
+  ["Des tablettes de chocolats au vrai cacao", "https://res.cloudinary.com/dcljc9muz/image/upload/c_scale,h_150,q_auto:good/v1525121430/cacao_l2ckpx.png"]
 ]
 
 category_list.each do |category|
   Category.create( name: category[0], pictoname: category[1] )
 end
+
