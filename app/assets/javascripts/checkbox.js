b956c4e2468da7b516b5184b31cd4711e75d8900
Thibault Lanery <thibaultlanery@gmail.com>
2019-05-06 13:05:24 +0200

    mise en forme home

--- app/assets/javascripts/checkbox.js
+++ app/assets/javascripts/checkbox.js
@@ -1,20 +1,4 @@
-// $( "div label" ).click(function() {
-//   $(this).toggleClass( "highlights" );
-// });
 
-// $('.card').click(function(){
-//     $('.hightlights', this).toggle(); // p00f
-// });
-
-
-// $('.img-rounded, h5').click( function(){
-//     if ( $(this).parent().hasClass('highlights') ) {
-//         $(this).parent().removeClass('highlights');
-//     } else {
-//         $(this).parent().removeClass('highlights');
-//         $(this).parent().addClass('highlights');
-//     }
-// });
 
 $('.checked').click( function(){
     if ( $(this).parent().hasClass('highlights') ) {
